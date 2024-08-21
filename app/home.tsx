7860ab7f174297e80e46b08df1a5c5b6d6278cc2
Pedro Moraes <pedro_moraes@yanmar.corp>
2024-08-21 10:44:34 -0300

    Envio do exercicio

--- app/home.tsx
+++ app/home.tsx
@@ -1,10 +1,17 @@
 import { View, Text } from "react-native"
 import { Link } from 'expo-router'
+import DisplayAnImage from "../components/styled-img"
 
 export default function Home(){
     return (
         <View>
             <Text>Login com sucesso!</Text>
+            <DisplayAnImage imageUri={require("https://i.pinimg.com/originals/97/21/05/972105c5a775f38cf33d3924aea053f1.jpg")} />
+            <Text> Usuário</Text>
+            <DisplayAnImage imageUri={require("https://w7.pngwing.com/pngs/206/889/png-transparent-document-file-format-directory-data-storage-computer-icons-computer-angle-text-rectangle.png")} />
+            <Text> Documentos</Text>
+            <DisplayAnImage imageUri={require("https://w7.pngwing.com/pngs/144/225/png-transparent-computer-icons-gear-wheel-others-miscellaneous-share-icon-cogwheel.png")} />
+            <Text> Configurações</Text>
             <Link href='/'>Voltar para home</Link>
         </View>
     )
--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import StyledInput from '../components/styled-input';
 import StyledButton from '../components/styled-button';
 import { router } from 'expo-router';
+import DisplayAnImage from '../components/styled-img';
 
 export default function Login() {
 
@@ -12,6 +13,7 @@ export default function Login() {
 
   return (
     <View style={styles.container}>
+      <DisplayAnImage imageUri={require("https://i.pinimg.com/originals/97/21/05/972105c5a775f38cf33d3924aea053f1.jpg")} />
       <Text>Login</Text>
       <StyledInput 
       placeholder='Digite seu login' 
@@ -37,5 +39,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16
   },
 });
